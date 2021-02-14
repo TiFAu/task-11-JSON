5d1a3164634d91c3987a9ab3bee0392b6828931e
TiFAu <turchin.anton2@gmail.com>
2021-02-15 01:15:12 +0200

    learning task-11-Json started

---
+++ JSON-01.js
@@ -0,0 +1,9 @@
+var obj = {
+   name: "sample",
+   type: "figure",
+   color: "green",
+   size: 200,
+   position: [ 250, 250 ]
+}
+JSON.stringify ( obj )
+//"{"name":"sample","type":"figure","color":"green","size":200,"position":[250,250]}"
